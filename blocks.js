12348057dd16dad32e182b905c15f9d63c19ca32
Dylan Schnabel <schnabel.dylan@gmail.com>
2017-04-03 19:33:22 -0500

    Upload blocks for testing

--- blocks.js
+++ blocks.js
@@ -4,7 +4,7 @@ Blockly.Blocks['turn_left'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Turn left")
-        .appendField(new Blockly.FieldDropdown([["a little","slight_left"], ["a lot","strong_left"], ["hard turn","hard_left"]]), "left");
+        .appendField(new Blockly.FieldDropdown([["30 degrees","slight_left"], ["60 degrees","strong_left"], ["90 degrees","hard_left"]]), "left");
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -18,7 +18,7 @@ Blockly.Blocks['turn_right'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Turn right")
-        .appendField(new Blockly.FieldDropdown([["a little","slight_right"], ["a lot","strong_right"], ["hard turn","hard_right"]]), "right");
+        .appendField(new Blockly.FieldDropdown([["30 degrees","slight_right"], ["60 degrees","strong_right"], ["90 degrees","hard_right"]]), "right");
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -31,7 +31,7 @@ Blockly.Blocks['turn_right'] = {
 Blockly.Blocks['move_forward_in'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("move_forward_inches")
+    .appendField("Move forward in inches")
         .appendField(new Blockly.FieldDropdown([["1","1_in"],["2","2_in"],["3","3_in"],["4","4_in"],["5","5_in"],["6","6_in"],["7","7_in"],["8","8_in"],["9","9_in"],["10","10_in"],["11","11_in"],]), "forward");
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
@@ -45,7 +45,7 @@ Blockly.Blocks['move_forward_in'] = {
 Blockly.Blocks['move_forward_ft'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("move_forward_feet")
+        .appendField("Move forward in feet")
         .appendField(new Blockly.FieldDropdown([["1","1_ft"],["2","2_ft"],["3","3_ft"],["4","4_ft"],["5","5_ft"],]), "forward");
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
@@ -60,13 +60,13 @@ Blockly.Blocks['move_forward_ft'] = {
 Blockly.Blocks['move_backward_in'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("move_backward_inches")
+        .appendField("Move backward in inches")
         .appendField(new Blockly.FieldDropdown([["1","1_in"],["2","2_in"],["3","3_in"],["4","4_in"],["5","5_in"],["6","6_in"],["7","7_in"],["8","8_in"],["9","9_in"],["10","10_in"],["11","11_in"],]), "backward");
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(135);
-    this.setTooltip('this is to move forward in inches');
+    this.setTooltip('this is to move backward in inches');
     this.setHelpUrl('');
   }
 };
@@ -74,38 +74,23 @@ Blockly.Blocks['move_backward_in'] = {
 Blockly.Blocks['move_backward_ft'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("move_backward_feet")
+        .appendField("Move backward in feet")
         .appendField(new Blockly.FieldDropdown([["1","1_ft"],["2","2_ft"],["3","3_ft"],["4","4_ft"],["5","5_ft"],]), "backward");
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(135);
-    this.setTooltip('this is to move forward in feet');
+    this.setTooltip('this is to move backward in feet');
     this.setHelpUrl('');
   }
 };
 
 
-Blockly.Blocks['do_donuts'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("do a donut! spin ")
-        .appendField(new Blockly.FieldDropdown([["right","spin_right"], ["left","spin_left"]]), "spin_type")
-        .appendField(new Blockly.FieldNumber('0'), "spinz")
-        .appendField("times!");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(240);
-    this.setTooltip('this is to reverse');
-    this.setHelpUrl('');
-  }
-};
 
 Blockly.Blocks['touch_sensor'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("touch_sensor")
+    .appendField("Touch Sensor: \nGo forward until touch")
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -118,7 +103,7 @@ Blockly.Blocks['touch_sensor'] = {
 Blockly.Blocks['distance_sensor'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("distance_from_object_cm")
+    .appendField("Distance Sensor")
         .appendField(new Blockly.FieldDropdown([["2","2_cm"],["4","4_cm"],["6","6_cm"],["8","8_cm"],["10","10_cm"],]), "distance");
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
@@ -128,23 +113,31 @@ Blockly.Blocks['distance_sensor'] = {
     this.setHelpUrl('');
   }
 };
-
-
-Blockly.Blocks['root_thang'] = {
+                                                
+Blockly.Blocks['do_donuts_clockwise'] = {
   init: function() {
-    this.setHelpUrl('http://www.example.com/');
-    this.setColour(120);
-    this.appendStatementInput("Start your shitty robot here")
-        .appendField("Header:");
-    this.appendValueInput("turn_left")
-        .setCheck("color")
-        .appendField("Text Color:");
-    this.appendValueInput("page_background")
-        .setCheck("color")
-        .appendField("Background Color:");
-    this.appendValueInput("page_paragraph")
-        .setCheck("String")
-        .appendField("Paragraph:");
-    this.setTooltip('');
+    this.appendDummyInput()
+        .appendField("Do Donuts Clockwise")
+        .appendField(new Blockly.FieldDropdown([["1","1 turn"], ["2","2 turns"], ["3","3 turns"], ["4","4 turns"], ["5","5 turns"]]), "spins")
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(240);
+    this.setTooltip('this is to do donuts clockwise');
+    this.setHelpUrl('');
   }
 };
+                                               
+Blockly.Blocks['do_donuts_counterclockwise'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Do Donuts Counterclockwise")
+        .appendField(new Blockly.FieldDropdown([["1","1 turn"], ["2","2 turns"], ["3","3 turns"], ["4","4 turns"], ["5","5 turns"]]), "spins")
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(240);
+    this.setTooltip('this is to do donuts counterclockwise');
+    this.setHelpUrl('');
+  }
+};                            
